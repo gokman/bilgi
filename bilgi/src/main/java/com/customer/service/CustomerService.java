8dd414ac5e5353d6aac6874996dfb8afdd43f2d4
bilgiapp <bilgiapp@2777289b-68e3-7a91-9207-f6cc2fb67c3a>
2012-05-27 08:36:22 +0000

    Detay sayfası icin ilgili servisler (g.kocaman)

--- bilgi/src/main/java/com/customer/service/CustomerService.java
+++ bilgi/src/main/java/com/customer/service/CustomerService.java
@@ -1,15 +1,18 @@
-package com.customer.service;
-
-import java.util.List;
-
-import com.customer.model.Customer;
-
-public interface CustomerService {
-
-		public void addCustomer(Customer customer);
-		
-		public List<Customer> listCustomers();
-		
-		public List<Customer> getByEmail (String email);
-		
-	}
+package com.customer.service; 
+ 
+import java.util.List; 
+ 
+import com.customer.model.Customer; 
+import com.lookup.model.LookupMst; 
+ 
+public interface CustomerService { 
+ 
+        public void addCustomer(Customer customer); 
+         
+        public List<Customer> listCustomers(); 
+         
+        public List<Customer> getByEmail (String email); 
+         
+        public List<Customer> getById(long id); 
+         
+    }
